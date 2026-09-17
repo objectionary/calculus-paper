7753c45748b95bfb7dc62cae6c1b90a218ba08ea
Yegor Bugayenko <yegor256@gmail.com>
2026-09-17 13:41:40 +0300

    Update Phino version to 0.0.133 in build scripts.

--- endless-to-tex.sh
+++ endless-to-tex.sh
@@ -9,8 +9,8 @@ dir=$1
 while IFS= read -r f; do
     e=$(basename "${f}")
     e=${e//.phi}
-    phino merge "${f}" runtime.phi | \
-        phino rewrite --normalize --focus=Q.ex "--expression=${e}" --max-depth=5 --max-cycles=1 \
+    phino --pin=0.0.133 merge "${f}" runtime.phi | \
+        phino --pin=0.0.133 rewrite --normalize --focus=Q.ex "--expression=${e}" --max-depth=5 --max-cycles=1 \
             --nonumber --sequence --compress "--meet-prefix=${e}" --output=latex \
             --flat --sweet
 done < <(find "${dir}" -name '*.phi' -type f | sort)
